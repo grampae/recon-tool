ae5cef644858632a079796c9f46f05dd5ff32567
grampae <36344197+grampae@users.noreply.github.com>
2023-01-03 19:21:13 -0500

    removed dir brute forcing

--- screenshot.py
+++ screenshot.py
@@ -23,12 +23,12 @@ import warnings
 import time
 #from tqdm import tqdm
 from rich.progress import Progress
+import logging
 
 #handle argument parsing
 parser = argparse.ArgumentParser(description="Screenshots r us", formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument("-t", dest="single", required=False, help="Single target, ex: http(s)://target.com")
 parser.add_argument("-l", dest="urlst", required=False, type=argparse.FileType("r", encoding="UTF-8"), help="List of urls")
-parser.add_argument("-d", dest="brlst", required=False, type=argparse.FileType("r", encoding="UTF-8"), help="Enable dir brute force with list")
 parser.add_argument("-p", dest="project", required=True, help="Project name")
 args = parser.parse_args()
 if len(sys.argv)==1:
@@ -37,7 +37,7 @@ if len(sys.argv)==1:
 project = args.project
 target = args.single
 urlst = args.urlst
-brlst = args.brlst
+
 if urlst:
 	urlist = [line.rstrip('\n') for line in urlst]
 #handle sig, cert, errors
@@ -48,6 +48,7 @@ def handler(signum, frame):
 signal.signal(signal.SIGINT, handler)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 warnings.filterwarnings("ignore", message="""Caught 'unbalanced parenthesis at position 119' compiling regex""", category=UserWarning )
+logging.captureWarnings(True)
 
 #install gecko webdriver if not already installed
 geckodriver_autoinstaller.install()
@@ -58,7 +59,6 @@ scrdir = prjdir+"/screenshots/"
 newlist = prjdir+'/newurlist.txt'
 tdir = "./template/"
 tname = "_layout.html"
-dirok = prjdir+"/dir200.txt"
 Path(scrdir).mkdir(parents=True, exist_ok=True)
 jdata = []
 wap = Wappalyzer.latest()
@@ -97,8 +97,7 @@ def doit(URL):
 			print("[-] screenshot timeout     - "+URL1)
 			driver.quit()
 			continue
-		except (NoSuchElementException, WebDriverException) as e:
-			print(e)
+		except (NoSuchElementException, WebDriverException):
 			print("[-] screenshot unreachable - "+URL1)
 			driver.quit()
 			continue
@@ -112,29 +111,24 @@ def doit(URL):
 
 #wappalyzer
 def gettech(URL1, domain):
-	response = requests.get(URL1, headers=headers, timeout=8)
-	wappage = WebPage.new_from_response(response)
-	wapres = wap.analyze_with_versions(wappage)
-	gettech.wapres1 = '  '.join(f'{value}' for value in wapres)
-	if gettech.wapres1 == "":
-		exit
-	else:
-		print("[#] wappalyzer response    - "+URL1+": "+str(gettech.wapres1))
+	try:
+		response = requests.get(URL1, headers=headers, timeout=8)
+		wappage = WebPage.new_from_response(response)
+		wapres = wap.analyze_with_versions(wappage)
+		gettech.wapres1 = '  '.join(f'{value}' for value in wapres)
+		if gettech.wapres1 == "":
+			exit
+		else:
+			print("[#] wappalyzer response    - "+URL1+": "+str(gettech.wapres1))
+	except:
+		print("[!] wappalyzer response    - "+URL1+": Failed")
 
 #handle threading if multiple urls or brute force
 def tpool():
 	print("[*] Saving url screenshot(s) in "+scrdir+" directory")
-	if target and brlst:
-		URL = target
-		brutef(URL)
-		mergeit()
-	elif target:
+	if target:
 		URL = target
 		doit(URL)
-	elif urlist and brlst:
-		URL = 'https://what.com'
-		brutef(URL)
-		mergeit()
 	elif urlst:
 		sworker(urlist)
 
@@ -147,72 +141,6 @@ def sworker(urlist):
 			future_to_url = {executor.submit(doit, URL): URL for URL in urlist}
 			for _ in as_completed(future_to_url):
 				progress.update(task2, advance=1)
-
-#brute force dirs threading
-def brutef(URL):
-	brlist = [line.rstrip('\n') for line in brlst]
-	LENGTH = len(brlist)
-	if urlist:
-		for URL in urlist:
-			with concurrent.futures.ThreadPoolExecutor(max_workers=75) as executor:
-				with Progress() as progress:
-					task1 = progress.add_task("[green]Discovering resources: "+URL, total=LENGTH)
-					future_to_dir = {executor.submit(checkpath, URL, DIR): DIR for DIR in brlist}
-					for _ in as_completed(future_to_dir):
-						progress.update(task1, advance=1)
-	else:
-		with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-			with Progress() as progress:
-				task1 = progress.add_task("[green]Discovering resources: "+URL, total=LENGTH)
-				future_to_dir = {executor.submit(checkpath, URL, DIR): DIR for DIR in brlist}
-				for _ in as_completed(future_to_dir):
-					progress.update(task1, advance=1)
-
-#brute force dirs
-def checkpath(URL, DIR):
-	try:
-		URL1=URL.strip()
-		DIR1 = DIR.strip()
-		durl = URL+"/"+DIR1
-		d = requests.get(durl, headers=headers, timeout=4)
-		if d.ok:
-			ds = len(d.content)
-			dsz = naturalsize(ds)
-			#print("[#] discovered url path    - :"+durl+" "+dsz)
-			with open(dirok, "a") as dirf:
-				dirf.write(durl+"\n")
-				dirf.close
-	except:
-		a=b
-
-#merge user provided url list with found urls
-def mergeit():
-	okok = Path(dirok)
-	if okok.exists() and urlst:
-		with open(dirok) as fp:
-			data = fp.read()
-		with open(urlst.name) as fp:
-			data2 = fp.read()
-		data += data2
-		with open(newlist, 'w') as fp:
-			fp.write(data)
-		with open(newlist, 'r') as fd:
-			urlist1 = fd.readlines()
-		urlist = [line.rstrip('\n') for line in urlist1]
-		sworker(urlist)
-	elif urlst:
-		sworker(urlist)
-	elif okok.exists() and target:
-		with open(dirok) as fp:
-			data = fp.read()
-		data += target
-		with open(newlist, 'w') as fp:
-			fp.write(data)
-		with open(newlist, 'r') as fd:
-			urlist = fd.readlines()
-		sworker(urlist)
-	else:
-		doit(URL)
 
 #render html with jinja
 def jin(directory, template_name, data):
